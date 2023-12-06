c9d6b2d3dba9dc80b74b3d335108e4e3b290ea57
hillro500 <85311805+hillro500@users.noreply.github.com>
2023-12-06 10:30:43 -0600

    Add files via upload

--- 2.py
+++ 2.py
@@ -5,3 +5,12 @@ Created on Wed Dec  6 09:57:22 2023
 @author: hillr
 """
 
+import numpy as np
+
+def solve_problem(problem):
+    answer = sum(100*np.random.rand(8))
+    return answer
+
+if __name__ == "__main__":
+    solve_problem("https://adventofcode.com/2023/day/2/input")
+
